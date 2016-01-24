d5de7e4e6f180e313f9d719ea2452554830eb2f0
Lefteris Karapetsas <lefteris@refu.co>
2016-01-24 15:47:41 +0000

    Add descriptions, node is now platform agnostic

--- bin/node_server.sh
+++ bin/node_server.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
+platform=`uname -i`
+NODE_EXEC=
+
+# choose the right node executable depending on platform
+case $platform in
+  x86_64)
+    NODE_EXEC=node
+    ;;
+  armv7l)
+    NODE_EXEC=arm-node
+    ;;
+  *)
+    echo "unknown platform "
+    ;;
+esac
+
 export NODE_PATH=./lib/node_modules:$NODE_PATH
-arm-node node_server.js
+$NODE_EXEC node_server.js
